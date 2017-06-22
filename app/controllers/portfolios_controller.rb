1bbabce29310b477876469530c6835bf164b131b
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 14:29:29 +0000

    Customized portfolio routes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -30,7 +30,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
      
     respond_to do |format|
-      if @blog.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
       else
         format.html { render :edit }
